74c79079a2e97c3bf5358bedc3887b3ef1bfae74
Andrés Cano <acanos@psl.com.co>
2020-11-20 21:02:08 -0500

    Feature/refactor (#3)
    * Improve code quality
    
    * Implement React Context into the application
    
    * Implement React Context into the application
    
    Co-authored-by: Andrés <andres.canos@perficient.com>

--- src/components/currencySelect/CryptoCurrencySelect.js
+++ src/components/currencySelect/CryptoCurrencySelect.js
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 import styled from "@emotion/styled";
 import { PropTypes } from 'prop-types';
+import { CurrencyContext } from "../../context";
 
 const Label = styled.label`
   font-family: "Bebas Neue", cursive;
@@ -22,22 +23,15 @@ const Select = styled.select`
   font-size: 1.2rem;
 `;
 
-export const CryptoCurrencySelect = ({ id, label, stateInicial, options, onChange }) => {
-  //state de nuestro custom hoock
-  const [state, setState] = useState(stateInicial ?? '');
-
-  const handleChange = (e) => {
-    const value = e.target.value;
-    setState(value);
-    onChange(value);
-  };
+export const CryptoCurrencySelect = ({ id, label, onChange }) => {
+  const { handleCryptoCurrencyChange, cryptoCurrency, crypoCurrencyData } = useContext(CurrencyContext);
 
   return (
     <div>
       <Label id={id}>{label}</Label>
-      <Select id={id} onChange={handleChange} value={state}>
+      <Select id={id} onChange={handleCryptoCurrencyChange} value={cryptoCurrency}>
         <option value="">- Seleccione -</option>
-        {options?.map((option) => (
+        {crypoCurrencyData?.map((option) => (
           <option key={option.CoinInfo.Id} value={option.CoinInfo.Name}>
             {option.CoinInfo.FullName}
           </option>
